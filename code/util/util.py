bdb99fbfdfc5ebdf12cb3fa52b9bb8b336b42ecc
gijswijnholds <gijswijnholds@gmail.com>
2019-12-02 10:11:44 +0100

    Fixed the fixtag function

--- code/util/util.py
+++ code/util/util.py
@@ -18,6 +18,23 @@ def fixtag_space(tag):
     return newtag
 
 
+def fixtag_fix(tag):
+    """Transform the tag of the corpus into our tags."""
+    if tag[:2] in ['NN', 'NP']:
+        newtag = 'NN'
+    elif tag[:2] == 'VV' or tag[:2] == 'VH':
+        newtag = 'VB'
+    elif tag[:2] == 'VB':
+        newtag = 'AV'
+    elif tag[:2] == 'RB':
+        newtag = 'RB'
+    elif tag[:2] == 'JJ':
+        newtag = 'JJ'
+    else:
+        newtag = '@'+tag
+    return newtag
+
+
 def fixtag_basis(tag):
     """Transform the tag of the corpus into our tags."""
     if tag[:2] in ['NN', 'NP']:
